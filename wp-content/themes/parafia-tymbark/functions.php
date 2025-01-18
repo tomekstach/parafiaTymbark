a6f1060300dddbfb6a887f0d2b2eb8bf6ed9ddc6
Tomek Stach <tomasz.stach@gmail.com>
2025-01-18 22:41:53 +0100

    Add info about dead people

--- wp-content/themes/parafia-tymbark/functions.php
+++ wp-content/themes/parafia-tymbark/functions.php
@@ -11,20 +11,73 @@
 /**
  * Define Constants
  */
-define( 'CHILD_THEME_PARAFIA_TYMBARK_VERSION', '1.0.0' );
+define('CHILD_THEME_PARAFIA_TYMBARK_VERSION', '1.0.0');
 
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
+function child_enqueue_styles()
+{
 
-	wp_enqueue_style( 'parafia-tymbark-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_PARAFIA_TYMBARK_VERSION, 'all' );
+    wp_enqueue_style('parafia-tymbark-theme-css', get_stylesheet_directory_uri() . '/style.css?v=20250118003', ['astra-theme-css'], CHILD_THEME_PARAFIA_TYMBARK_VERSION, 'all');
 
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 15);
 
-add_action( 'acf/init', 'set_acf_settings' );
-function set_acf_settings() {
-    acf_update_setting( 'enable_shortcode', true );
+add_action('acf/init', 'set_acf_settings');
+function set_acf_settings()
+{
+    acf_update_setting('enable_shortcode', true);
 }
+
+function display_pogrzeb_posts()
+{
+    $args = [
+        'post_type'      => 'pogrzeb',
+        'posts_per_page' => -1,
+    ];
+
+    $pogrzeb_query = new WP_Query($args);
+
+    $output = '';
+
+    if ($pogrzeb_query->have_posts()) {
+        $counter = $pogrzeb_query->post_count;
+        echo $counter;
+        $i      = 1;
+        $output = '
+    <div class="wp-block-uagb-container uagb-block-a31af704">
+        <div class="wp-block-uagb-info-box uagb-block-79996f03 uagb-infobox__content-wrap  uagb-infobox-icon-above-title uagb-infobox-image-valign-middle wp-block-uagb-info-box--has-margin">
+            <div class="uagb-infobox-margin-wrapper">
+                <div class="uagb-ifb-content">
+                    <div class="uagb-ifb-image-content"><img decoding="async" data-src="https://parafia.astosoft.pl/wp-content/uploads/2024/12/christian-cross.svg" alt="" width="40" height="40" loading="lazy" src="https://parafia.astosoft.pl/wp-content/uploads/2024/12/christian-cross.svg" class=" lazyloaded" style="--smush-placeholder-width: 40px; --smush-placeholder-aspect-ratio: 40/40;"><noscript><img decoding="async" src="https://parafia.astosoft.pl/wp-content/uploads/2024/12/christian-cross.svg" alt="" width="40" height="40" loading="lazy"/></noscript></div>
+                    <div class="uagb-ifb-title-wrap">
+                        <h2 class="uagb-ifb-title">Informacja o zmarłych</h2>
+                    ';
+        while ($pogrzeb_query->have_posts()) {
+            $pogrzeb_query->the_post();
+            $content = get_the_content();
+            $content = apply_filters('the_content', $content);
+            $output .= '<div>' . $content . '</div>';
+
+            if ($i < $counter) {
+                $output .= '<div class="wp-block-uagb-separator uagb-block-5ce44d1c"><div class="wp-block-uagb-separator__inner" style="--my-background-image:"></div></div>';
+            }
+
+            $i++;
+        }
+        $output .= '
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+    }
+
+    wp_reset_postdata();
+
+    return $output;
+}
+
+add_shortcode('pogrzeb_posts', 'display_pogrzeb_posts');
